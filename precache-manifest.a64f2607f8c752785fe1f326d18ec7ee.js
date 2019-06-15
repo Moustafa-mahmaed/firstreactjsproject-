f1cba6ea558d53635ab2c5ec8020576127821101
moustafamahamedshamaa <moustafamahamedshamaa@gmail.com>
2019-06-15 17:42:48 +0200

    Updates

---
+++ precache-manifest.a64f2607f8c752785fe1f326d18ec7ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42941067b5fee3aaacec3c7dece17a00",
+    "url": "/firstreactjsproject/index.html"
+  },
+  {
+    "revision": "a2a0f1ca0fe8bdccf2b9",
+    "url": "/firstreactjsproject/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1fd169eea981a3ba6d8e",
+    "url": "/firstreactjsproject/static/js/2.b8abf315.chunk.js"
+  },
+  {
+    "revision": "a2a0f1ca0fe8bdccf2b9",
+    "url": "/firstreactjsproject/static/js/main.3b6d88b9.chunk.js"
+  },
+  {
+    "revision": "98354edad90d0245f34e",
+    "url": "/firstreactjsproject/static/js/runtime~main.b9737f66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstreactjsproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
